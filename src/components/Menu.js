ec6ca72bfe1182a4a1b1c765413b360819405005
Herman White <phoenixquint@gmail.com>
2018-10-16 23:57:33 -0400

    Added styling and functions for menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,20 +3,66 @@ import './Menu.css';
 import { locations } from '../data/locations';
 
 export default class Menu extends Component {
+  state = {
+    value: '',
+    venues: [],
+    links: [],
+    activeLink: null,
+  }
+
+  onClick = (e) => {
+    e.preventDefault();
+    e.target.classList.add('active-link');
+    this.setState({ activeLink: e.target })
+  }
+
+  handleChange = (e) => {
+    const { venues } = this.state;
+    const filteredLinks = venues.filter(venue => {
+      return venue.name.toLowerCase().includes(e.target.value)
+    })
+    
+    this.setState({
+      value: e.target.value,
+      links: filteredLinks
+    })
+  }
+
+  componentDidMount = () => {
+    this.setState({ venues: locations, links: locations })
+  }
+
+  componentDidUpdate = (prevProps, prevState) => {
+    // removes active-link class
+    if (prevState.activeLink === null) {
+      // no active link
+    } else if (this.state.activeLink !== prevState.activeLink) {
+      prevState.activeLink.classList.remove('active-link');
+    }
+  }
+
   render() {
+    const { links } = this.state;
       // console.log(locations)
       // TODO: make these links clickable, and style them
     return (
       <div className='side-menu'>
-        <ul className='location-list'>
-          {locations.map(location => {
+      <input type="text" 
+      className='search-box'
+      placeholder="Search.."
+      onChange={this.handleChange}
+      value={this.state.value}/>
+          {links.map(location => {
             return (
-              <li key={location.venueId}>
+              <a 
+              href={'/'} 
+              key={location.venueId}
+              onClick={this.onClick}>
+              
                 {location.name}
-              </li>
+              </a>
             )
           })}
-        </ul>
       </div>
     )
   }
